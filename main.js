64ad2875413ce3662dc70309e70ef2ffa559e41f
Edson <esinuhe_cazares@hotmail.com>
2023-01-31 18:31:20 -0600

    cambio en la funcion validarTexto, modificación en la declaración de la sentencia if

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function validarTexto(){
     let textoEscrito = document.querySelector(".texto-ingresado").value;
     let validador = textoEscrito.match(/^[a-z]*$/);
 
-    if(!validador || validador === 0) {
+    if(!validador) {
         Swal.fire({
             icon: 'error',
             iconColor:'red',
